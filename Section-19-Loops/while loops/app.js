d091c272aa741685dcf34919ae60e9ace20d156c
mmott18 <matthewmott7@gmail.com>
2022-01-20 20:08:40 -0500

    Finished section 19

--- Section-19-Loops/while loops/app.js
+++ Section-19-Loops/while loops/app.js
@@ -16,11 +16,34 @@
 // 	}
 // }
 
-let input = prompt('Enter something:');
-while (true) {
-	input = prompt(input);
-	if (input.toLowerCase === 'quit') {
+// let input = prompt('Enter something:');
+// while (true) {
+// 	input = prompt(input);
+// 	if (input.toLowerCase === 'quit') {
+// 		break;
+// 	}
+// }
+// console.log('You win!');
+
+// Guessing Game
+let maxNum = parseInt(prompt('Enter a maximum number:'));
+while (!maxNum) {
+	maxNum = parseInt(prompt('Enter a VALID maximum number:'));
+}
+let winningNum = Math.floor(Math.random() * maxNum) + 1;
+let numGuess = 1;
+let guess = prompt('Enter your guess:');
+
+while (parseInt(guess) !== winningNum) {
+	numGuess++;
+	if (guess < winningNum) {
+		guess = prompt('Too low. Guess again:');
+	} else if (guess > winningNum) {
+		guess = prompt('Too high. Guess again:');
+	} else if (guess.toLowerCase() === 'q') {
 		break;
+	} else {
+		guess = prompt('Invalid entry. Try again:');
 	}
 }
-console.log('You win!');
+console.log(`Congratulations, you won after ${numGuess} guesses!`);
