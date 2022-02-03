9645795876dbeefaaa10380a2e8508fb027a81b7
mmott18 <matthewmott7@gmail.com>
2022-02-02 20:40:23 -0500

    Continued section 24 - mostly console work and note taking

--- Bringing It All Together/Section-24-Intro-to-the-DOM/Manipulating/app.js
+++ Bringing It All Together/Section-24-Intro-to-the-DOM/Manipulating/app.js
@@ -6,15 +6,15 @@ const allLinks = document.querySelectorAll('a');
 // 		'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80';
 // }
 
-const squareImages = document.getElementsByClassName('square');
+// const squareImages = document.getElementsByClassName('square');
 
-for (let img of squareImages) {
-	img.src =
-		'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80';
-}
+// for (let img of squareImages) {
+// 	img.src =
+// 		'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80';
+// }
 
 for (let link of allLinks) {
-	link.style.color = 'rgb(0, 108, 134)';
-	link.style.textDecorationColor = 'magenta';
-	link.style.textDecorationStyle = 'wavy';
+	link.style.color = 'blue';
+	link.style.textDecorationColor = 'navy';
+	link.style.textDecorationStyle = 'dashed';
 }
