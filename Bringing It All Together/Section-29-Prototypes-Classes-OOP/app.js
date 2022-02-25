b740f8500faf15ce9d2203ba094f07d567ca8528
mmott18 <matthewmott7@gmail.com>
2022-02-25 17:55:33 -0500

    Started section 29 - OOP

---
+++ Bringing It All Together/Section-29-Prototypes-Classes-OOP/app.js
@@ -0,0 +1,10 @@
+// below adds an additional method that we define to the
+// String prototype object
+// String.prototype.yell = function() {
+// 	return `It's ${this.toUpperCase()}`;
+// };
+
+// these don't do anything yet as we have not defined
+// the logic for the object/prototype
+const navColor = new Color('carrot', [ 230, 126, 34 ]);
+const logoColor = new Color('emerald', [ 46, 204, 113 ]);
