78d747ca352f9d56363b30ef31854f3cff6e1f59
mmott18 <matthewmott7@gmail.com>
2022-03-23 16:33:03 -0400

    Continued section 35 - RESTful routes

--- Backend/Section-35-RESTful-Routes/RESTDemo/index.js
+++ Backend/Section-35-RESTful-Routes/RESTDemo/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+	{
+		id: 1,
+		username: 'Todd',
+		comment: 'lol'
+	},
+	{
+		id: 2,
+		username: 'Skyler',
+		comment: 'I like to go birdwatching with my dog'
+	},
+	{
+		id: 3,
+		username: 'Sk8erBoi',
+		comment: 'Plz delete your account, Todd'
+	},
+	{
+		id: 4,
+		username: 'onlysayswoof',
+		comment: 'woof woof woof'
+	}
+];
 
 app.get('/tacos', (req, res) => {
 	res.send('GET /tacos response');
@@ -13,11 +39,25 @@ app.post('/tacos', (req, res) => {
 	res.send(`You ordered ${qty} ${meat} tacos.`);
 });
 
-app.get('/comments', (req, res) => {});
+app.get('/comments', (req, res) => {
+	res.render('comments/index', { comments });
+});
 
-app.post('/comments', (req, res) => {});
+app.get('/comments/new', (req, res) => {
+	res.render('comments/new', { comments });
+});
 
-app.get('/comments/:id', (req, res) => {});
+app.post('/comments', (req, res) => {
+	const { username, comment } = req.body;
+	comments.push({ username, comment });
+	res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+	const { id } = req.params;
+	const comment = comments.find((c) => c.id === parseInt(id));
+	res.render('comments/show', { comment });
+});
 
 app.patch('/comments/:id', (req, res) => {});
 
