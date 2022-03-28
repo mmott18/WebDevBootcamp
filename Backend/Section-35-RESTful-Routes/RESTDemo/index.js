ca1167b0e15605380f79d6fd38c1c522c2ab7e85
mmott18 <matthewmott7@gmail.com>
2022-03-28 16:25:16 -0400

    Finished section 35 - RESTful routes

--- Backend/Section-35-RESTful-Routes/RESTDemo/index.js
+++ Backend/Section-35-RESTful-Routes/RESTDemo/index.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const comments = [
+let comments = [
 	{
-		id: 1,
+		id: uuid(),
 		username: 'Todd',
 		comment: 'lol'
 	},
 	{
-		id: 2,
+		id: uuid(),
 		username: 'Skyler',
 		comment: 'I like to go birdwatching with my dog'
 	},
 	{
-		id: 3,
+		id: uuid(),
 		username: 'Sk8erBoi',
 		comment: 'Plz delete your account, Todd'
 	},
 	{
-		id: 4,
+		id: uuid(),
 		username: 'onlysayswoof',
 		comment: 'woof woof woof'
 	}
@@ -49,19 +52,40 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
 	const { username, comment } = req.body;
-	comments.push({ username, comment });
+	comments.push({ username, comment, id: uuid() });
 	res.redirect('/comments');
 });
 
 app.get('/comments/:id', (req, res) => {
 	const { id } = req.params;
-	const comment = comments.find((c) => c.id === parseInt(id));
+	const comment = comments.find((c) => c.id === id);
 	res.render('comments/show', { comment });
 });
 
-app.patch('/comments/:id', (req, res) => {});
+app.patch('/comments/:id', (req, res) => {
+	try {
+		const { id } = req.params;
+		const updatedComment = req.body.comment;
+		const foundComment = comments.find((c) => c.id === id);
+		foundComment.comment = updatedComment;
+		res.redirect('/comments');
+	} catch (e) {
+		res.send(`Comment not found: ${e}`);
+	}
+});
 
-app.delete('/comments/:id', (req, res) => {});
+app.get('/comments/:id/edit', (req, res) => {
+	const { id } = req.params;
+	const comment = comments.find((c) => c.id === id);
+	console.log(comment);
+	res.render('comments/edit', { comment });
+});
+
+app.delete('/comments/:id', (req, res) => {
+	const { id } = req.params;
+	comments = comments.filter((c) => c.id !== id);
+	res.redirect('/comments');
+});
 
 app.listen(3000, () => {
 	console.log('On port 3000');
