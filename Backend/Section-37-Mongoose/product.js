cae70123485b1ea84e68667593d048ce40dcf766
mmott18 <matthewmott7@gmail.com>
2022-04-05 14:28:35 -0400

    Continued section 37 - Mongoose

--- Backend/Section-37-Mongoose/product.js
+++ Backend/Section-37-Mongoose/product.js
@@ -11,20 +11,77 @@ mongoose
 const productSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: true
+		required: true,
+		maxlength: 20
 	},
 	price: {
 		type: Number,
-		required: true
+		required: true,
+		min: [ 0, 'Price must be above 0' ]
 	},
 	isOnSale: {
-		type: Boolean
+		type: Boolean,
+		default: false
+	},
+	categories: {
+		type: [ String ],
+		default: [ 'cycling' ]
+	},
+	qty: {
+		online: {
+			type: Number,
+			default: 0
+		},
+		inStore: {
+			type: Number,
+			default: 0
+		}
+	},
+	size: {
+		type: String,
+		enum: [ 'S', 'M', 'L' ]
 	}
 });
 
+// productSchema.methods.greet = function() {
+// 	console.log(`Hello from ${this.name}`);
+// };
+
+productSchema.methods.toggleSale = async function() {
+	this.isOnSale = !this.isOnSale;
+	return this.save();
+};
+
+productSchema.methods.reducePrice = async function() {
+	this.price = this.price * 0.8;
+	return this.save();
+};
+
+productSchema.methods.addCategory = async function(newCat) {
+	this.categories.push(newCat);
+	return this.save();
+};
+
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Racing Bike', price: 1199, color: 'Red' });
+const findProduct = async () => {
+	const foundProduct = await Product.findOne({ name: 'Bike Helmet' });
+	console.log(foundProduct);
+	await foundProduct.toggleSale();
+	await foundProduct.reducePrice();
+	await foundProduct.addCategory('Sale!');
+	console.log(foundProduct);
+};
+
+findProduct();
+
+const bike = new Product({
+	name: 'Recreational Bike',
+	price: 399.99,
+	isOnSale: true,
+	categories: [ 'Cycling' ],
+	size: 'M'
+});
 bike
 	.save()
 	.then((data) => {
@@ -33,3 +90,11 @@ bike
 	.catch((err) => {
 		console.log(`Failed: ${err}`);
 	});
+
+// Product.findOneAndUpdate({ name: 'Tire Pump ' }, { price: -10.99 }, { new: true, runValidators: true })
+// 	.then((data) => {
+// 		console.log(`Success: ${data}`);
+// 	})
+// 	.catch((err) => {
+// 		console.log(`Failed: ${err}`);
+// 	});
