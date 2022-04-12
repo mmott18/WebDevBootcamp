a802d60e647fa3b409617120eaf49f89e4a50c97
mmott18 <matthewmott7@gmail.com>
2022-04-12 15:53:41 -0400

    Finished section 38 - Mongoose with Express

--- Backend/Section-38-Mongoose-with-Express/index.js
+++ Backend/Section-38-Mongoose-with-Express/index.js
@@ -20,19 +20,27 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const categories = [ 'fruit', 'vegetable', 'dairy', 'fungi' ];
+
 app.get('/products', async (req, res) => {
-	const products = await Product.find({});
-	res.render('products/index', { products });
+	const { category } = req.query;
+	if (category) {
+		const products = await Product.find({ category });
+		res.render('products/index', { products, category });
+	} else {
+		const products = await Product.find({});
+		res.render('products/index', { products, category: 'All' });
+	}
 });
 
 app.get('/products/new', (req, res) => {
-	res.render('products/new');
+	res.render('products/new', { categories });
 });
 
 app.get('/products/:id/edit', async (req, res) => {
 	const { id } = req.params;
 	const product = await Product.findById(id);
-	res.render('products/edit', { product });
+	res.render('products/edit', { product, categories });
 });
 
 app.put('/products/:id', async (req, res) => {
@@ -51,6 +59,12 @@ app.get('/products/:id', async (req, res) => {
 	const { id } = req.params;
 	const product = await Product.findById(id);
 	res.render('products/show', { product });
+});
+
+app.delete('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const deletedProduct = await Product.findByIdAndDelete(id);
+	res.redirect('/products');
 });
 
 app.listen(3000, () => {
