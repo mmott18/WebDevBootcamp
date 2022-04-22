97ed5ffd5ecb0f5a7848b2f590d51aa4c537c188
mmott18 <matthewmott7@gmail.com>
2022-04-22 14:52:17 -0400

    YelpCamp - started adding Bootstrap for styling

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -16,6 +17,7 @@ db.once('open', () => {
 	console.log('Database connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
